8ce0fb37794caefb468db53db0c7af1603a0a391
Martin <martin.ricard2008@gmail.com>
2026-03-31 10:40:11 +0200

    Mon troisième Commit

---
+++ src/Form/UtilisateurFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UtilisateurFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('prenom')
+            ->add('profileImage')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('isVerified')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
